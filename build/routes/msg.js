28cce44e89c64c0938da195b67f60ff10d5722ab
estenium <estenium@proton.me>
2025-10-06 17:16:20 +0000

    Update postMessage route validation

--- build/routes/msg.js
+++ build/routes/msg.js
@@ -14,13 +14,8 @@ router.post('/send', telegram_rate_limiter_1.telegramMessageLimiter, [
         .withMessage('Message must be a string')
         .notEmpty()
         .withMessage('Message cannot be empty')
-        .isLength({ max: 4096 })
-        .withMessage('Message is too long (max 4096 characters)'),
-    (0, express_validator_1.body)('token')
-        .isString()
-        .withMessage('Token must be a string')
-        .notEmpty()
-        .withMessage('Token is required'),
+        .isLength({ max: 2000 })
+        .withMessage('Message is too long (max 2000 characters)'),
 ], msg_1.postMessage);
 router.use(error_1.handleHttpError);
 exports.default = router;
